d3e27efb669c9ba282101a49ad6f33808723d3e2
Espirak <espira@qq.com>
2025-05-01 03:27:42 +0800

    f

--- programs/memo/instructions.go
+++ programs/memo/instructions.go
@@ -17,10 +17,10 @@ package memo
 import (
 	"bytes"
 	"fmt"
+	ag_solanago "github.com/Espirak/solana-go"
+	ag_text "github.com/Espirak/solana-go/text"
 	"github.com/davecgh/go-spew/spew"
 	ag_binary "github.com/gagliardetto/binary"
-	ag_solanago "github.com/gagliardetto/solana-go"
-	ag_text "github.com/gagliardetto/solana-go/text"
 	"github.com/gagliardetto/treeout"
 )
 
